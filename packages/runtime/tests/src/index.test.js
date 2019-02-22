9e909ba6443e4dff1cf40a401baac415f2e05574
Manuel Vila <mvila@3base.com>
2019-02-22 09:32:23 +0900

    Implement more tests

--- packages/runtime/tests/src/index.test.js
+++ packages/runtime/tests/src/index.test.js
@@ -1,8 +1,8 @@
 import {invokeQuery} from '../../..';
 
 describe('@deepr/runtime', () => {
-  describe('simple queries', () => {
-    test('get attributes of an object', async () => {
+  describe('Simple queries', () => {
+    test('Get attributes of an object', async () => {
       expect(
         await invokeQuery(
           {movie: {title: 'Inception', year: 2010, country: 'USA'}},
@@ -13,7 +13,7 @@ describe('@deepr/runtime', () => {
       });
     });
 
-    test('call a method on a collection', async () => {
+    test('Call a method on a collection', async () => {
       expect(
         await invokeQuery(
           {
@@ -33,7 +33,7 @@ describe('@deepr/runtime', () => {
       });
     });
 
-    test('get attributes of collection elements', async () => {
+    test('Get attributes of collection elements', async () => {
       expect(
         await invokeQuery(
           {movies: [{title: 'Inception', year: 2010}, {title: 'The Matrix', year: 1999}]},
@@ -42,7 +42,7 @@ describe('@deepr/runtime', () => {
       ).toEqual({movies: [{title: 'Inception', year: 2010}, {title: 'The Matrix', year: 1999}]});
     });
 
-    test('call a method on a collection and get attributes of collection elements', async () => {
+    test('Call a method on a collection and get attributes of collection elements', async () => {
       expect(
         await invokeQuery(
           {
@@ -67,8 +67,8 @@ describe('@deepr/runtime', () => {
     });
   });
 
-  describe('parameters', () => {
-    test('call a function with some parameters', async () => {
+  describe('Parameters', () => {
+    test('Call a function with some parameters', async () => {
       expect(
         await invokeQuery(
           {
@@ -85,8 +85,121 @@ describe('@deepr/runtime', () => {
     });
   });
 
-  describe('fault-tolerant queries', () => {
-    test('get attributes of an object', async () => {
+  describe('Keys', () => {
+    test('"key" variant', async () => {
+      expect(
+        await invokeQuery({movie: {title: 'Inception', year: 2010}}, {movie: {title: true}})
+      ).toEqual({movie: {title: 'Inception'}});
+    });
+
+    test('"sourceKey=>targetKey" variant', async () => {
+      expect(
+        await invokeQuery(
+          {
+            movies({filter: {genre}}) {
+              if (genre === 'action') {
+                return [{title: 'Inception'}, {title: 'The Matrix'}];
+              }
+              if (genre === 'drama') {
+                return [{title: 'Forrest Gump'}];
+              }
+            }
+          },
+          {
+            'movies=>actionMovies': {'()': {filter: {genre: 'action'}}, '=>': [{title: true}]},
+            'movies=>dramaMovies': {'()': {filter: {genre: 'drama'}}, '=>': [{title: true}]}
+          }
+        )
+      ).toEqual({
+        actionMovies: [{title: 'Inception'}, {title: 'The Matrix'}],
+        dramaMovies: [{title: 'Forrest Gump'}]
+      });
+    });
+
+    test('"=>targetKey" variant', async () => {
+      expect(
+        await invokeQuery(
+          {
+            movies: {
+              * [Symbol.iterator]() {
+                yield {title: 'Inception', year: 2010};
+                yield {title: 'The Matrix', year: 1999};
+              },
+              count() {
+                return 2;
+              }
+            }
+          },
+          {movies: {count: true, '=>items': [{title: true}]}}
+        )
+      ).toEqual({
+        movies: {
+          count: 2,
+          items: [{title: 'Inception'}, {title: 'The Matrix'}]
+        }
+      });
+    });
+
+    test('"sourceKey=>" variant', async () => {
+      expect(
+        await invokeQuery({movie: {title: 'Inception', year: 2010}}, {movie: {'title=>': true}})
+      ).toEqual({movie: 'Inception'});
+    });
+
+    test('"=>" variant', async () => {
+      const object = {
+        movie({id}) {
+          if (id === 'cjrts72gy00ik01rv6eins4se') {
+            return {title: 'Inception', year: 2010, country: 'USA'};
+          }
+        }
+      };
+
+      expect(
+        await invokeQuery(object, {
+          movie: {'()': {id: 'cjrts72gy00ik01rv6eins4se'}, '=>': {title: true, year: true}}
+        })
+      ).toEqual({movie: {title: 'Inception', year: 2010}});
+
+      expect(
+        await invokeQuery(object, {
+          movie: {'()': {id: 'cjrts72gy00ik01rv6eins4se'}, title: true, year: true}
+        })
+      ).toEqual({movie: {title: 'Inception', year: 2010}});
+    });
+  });
+
+  describe('Values', () => {
+    test('Boolean `true`', async () => {
+      expect(
+        await invokeQuery(
+          {movie: {title: 'Inception', year: 2010, country: 'USA'}},
+          {movie: {title: true, year: true}}
+        )
+      ).toEqual({movie: {title: 'Inception', year: 2010}});
+    });
+
+    test('Object', async () => {
+      expect(
+        await invokeQuery(
+          {movie: {title: 'Star Wars', director: {name: 'Georges Lucas', popularity: 70}}},
+          {movie: {director: {name: true}}}
+        )
+      ).toEqual({movie: {director: {name: 'Georges Lucas'}}});
+    });
+
+    test('Array', async () => {
+      expect(
+        await invokeQuery(
+          {movies: [{title: 'Inception'}, {title: 'The Matrix'}, {title: 'Forest Gump'}]},
+          {movies: [{title: true}]}
+        )
+      ).toEqual({movies: [{title: 'Inception'}, {title: 'The Matrix'}, {title: 'Forest Gump'}]});
+    });
+  });
+
+  describe('Fault-tolerant queries', () => {
+    test('Get attributes of an object', async () => {
       await expect(
         invokeQuery(
           {movie: {title: 'Inception'}},
@@ -106,7 +219,7 @@ describe('@deepr/runtime', () => {
       ).resolves.toEqual({movie: {title: 'Inception'}});
     });
 
-    test('call a method on an object', async () => {
+    test('Call a method on an object', async () => {
       await expect(
         invokeQuery(
           {movie: {title: 'Inception'}},
@@ -120,6 +233,54 @@ describe('@deepr/runtime', () => {
           {movie: {title: true, 'actors?': {'()': {sort: {by: 'popularity'}}}}}
         )
       ).resolves.toEqual({movie: {title: 'Inception'}});
+    });
+  });
+
+  describe('Fault-tolerant queries', () => {
+    test('Get attributes of an object', async () => {
+      await expect(
+        invokeQuery(
+          {movie: {title: 'Inception'}},
+          {movie: {title: true, director: {fullName: true}}}
+        )
+      ).rejects.toThrow();
+
+      await expect(
+        invokeQuery(
+          {movie: {title: 'Inception'}},
+          {movie: {title: true, 'director?': {fullName: true}}}
+        )
+      ).resolves.toEqual({movie: {title: 'Inception'}});
+
+      await expect(
+        invokeQuery({movie: {title: 'Inception'}}, {movie: {title: true, director: true}})
+      ).resolves.toEqual({movie: {title: 'Inception'}});
+    });
+  });
+
+  describe('Async execution', () => {
+    test('Async fetch', async () => {
+      expect(
+        await invokeQuery(
+          {
+            async movie({id}) {
+              await sleep(50);
+              if (id === 'cjrts72gy00ik01rv6eins4se') {
+                return {title: 'Inception', year: 2010, country: 'USA'};
+              }
+            }
+          },
+          {
+            movie: {'()': {id: 'cjrts72gy00ik01rv6eins4se'}, '=>': {title: true, year: true}}
+          }
+        )
+      ).toEqual({movie: {title: 'Inception', year: 2010}});
+
+      function sleep(duration) {
+        return new Promise(resolve => {
+          setTimeout(resolve, duration);
+        });
+      }
     });
   });
 });
