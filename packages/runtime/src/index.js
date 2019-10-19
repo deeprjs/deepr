3df495b8ef6a21d7ed8c8efcf6aa77bd8c95c4b5
Manuel Vila <mvila@3base.com>
2019-10-20 08:24:27 +0900

    runtime: Make invokeQuery() accepts multiple queries

--- packages/runtime/src/index.js
+++ packages/runtime/src/index.js
@@ -1,3 +1,5 @@
+import {possiblyAsync} from 'possibly-async';
+
 import {parseQuery} from './parser';
 import {invokeExpression} from './runtime';
 
@@ -12,6 +14,19 @@ export function invokeQuery(
 
   if (query === undefined) {
     throw new Error(`'query' parameter is missing`);
+  }
+
+  if (Array.isArray(query)) {
+    const queries = query;
+    return possiblyAsync.map(queries, function (query) {
+      return invokeQuery(root, query, {
+        context,
+        ignoreKeys,
+        acceptKeys,
+        ignoreBuiltInKeys,
+        authorizer
+      });
+    });
   }
 
   return invokeExpression(root, parseQuery(query, {ignoreKeys, acceptKeys, ignoreBuiltInKeys}), {
